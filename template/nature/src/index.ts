cfda267aae95eb389d82f622bac79d051a5bae6a
Lauri Kurki <lauri.kurki@siili.com>
2020-10-28 15:10:19 +0200

    Implement templates executor/nature

--- template/nature/src/index.ts
+++ template/nature/src/index.ts
@@ -1,1 +1,54 @@
+import fs from 'fs'
+import { Pool } from 'pg'
 import mutate, { Nature } from 'ts-nature'
+import * as io from './io'
+
+if (!Object.keys(io.CONFIG).every((k) => (io.CONFIG as any)[k])) {
+    console.error(
+        'Invalid config, missing environment variables:',
+        JSON.stringify(io.CONFIG)
+    )
+
+    process.exit(1)
+}
+
+const CREATE_VERSION_TABLE = `
+create table if not exists evolution_version as
+  select 0 as id
+`
+
+const GET_VERSION_NUMBER = `
+select max(id) as id from evolution_version
+`
+
+const UPDATE_VERSION_NUMBER = `
+update evolution_version set id = $1
+`
+
+const query = <T extends {}>(
+    pool: Pool,
+    sql: string,
+    ...args: any[]
+): Promise<T[]> =>
+    pool.connect().then((client) =>
+        client
+            .query<T>(sql, args)
+            .then((res) => res.rows)
+            .finally(() => client.release())
+    )
+
+const POOL = io.getPool()
+
+const nature: Nature<string> = {
+    schema: io.readFiles(io.CONFIG.SCHEMA_DIR)[0],
+    evolutions: io.readFiles(io.CONFIG.EVOLUTION_DIR),
+    devolutions: io.readFiles(io.CONFIG.DEVOLUTION_DIR),
+    execute: (sql) => query(POOL, sql),
+    getVersion: () =>
+        query<{ id: number }>(POOL, GET_VERSION_NUMBER).then(
+            (rows) => rows[0].id
+        ),
+    updateVersion: (version) => query(POOL, UPDATE_VERSION_NUMBER, version),
+}
+
+query(POOL, CREATE_VERSION_TABLE).then(() => mutate(nature))
---
+++ template/nature/src/io.ts
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import { Pool } from 'pg'
+
+export const CONFIG = {
+    SCHEMA_DIR: process.env.SCHEMA_DIR,
+    EVOLUTION_DIR: process.env.EVOLUTION_DIR,
+    DEVOLUTION_DIR: process.env.DEVOLUTION_DIR,
+    POSTGRES_PASSWORD_FILE: process.env.POSTGRES_PASSWORD_FILE,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+}
+
+export const readFiles = (dirname: string): string[] =>
+    fs
+        .readdirSync(dirname)
+        .map((filename) => fs.readFileSync(`${dirname}/${filename}`).toString())
+
+export const getPool = (): Pool => {
+    const password = fs
+        .readFileSync(CONFIG.POSTGRES_PASSWORD_FILE, 'utf8')
+        .replace(/[\r\n]/g, '')
+
+    return new Pool({
+        user: CONFIG.POSTGRES_USER,
+        password: password,
+        database: CONFIG.POSTGRES_DB,
+        host: CONFIG.POSTGRES_HOST,
+    })
+}
